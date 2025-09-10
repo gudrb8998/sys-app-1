f84c8737c46d8827ad94462dfd56b81fdb13a08d
hks0902 <hks0902@lguplus.co.kr>
2025-09-10 21:08:54 +0900

    질문 문장 수정

--- src/Question.js
+++ src/Question.js
@@ -23,10 +23,10 @@ const Question = () => {
   return (
     <div className="question-container">
       <div className="question-card">
-        <h2>1. 나는 OOO한 관객이다.</h2>
+        <h2>1. 나는 OOO(한) 관객이다.</h2>
         <input
           type="text"
-          placeholder="답변을 입력하세요.."
+          placeholder="답변을 입력하세요."
           value={audienceAnswer}
           onChange={(e) => setAudienceAnswer(e.target.value)}
         />
@@ -37,7 +37,7 @@ const Question = () => {
         <h2>2. 내가 제일 재미있게 본 공연은 OOO이다.</h2>
         <input
           type="text"
-          placeholder="답변을 입력하세요.."
+          placeholder="답변을 입력하세요."
           value={performanceAnswer}
           onChange={(e) => setPerformanceAnswer(e.target.value)}
         />
